332ad0a8908e992c69a42e59666c66845d18acfa
Prince <princegupta1455@gmail.com>
2025-03-18 00:45:54 +0530

    how to add write js in html

--- 2.CreateRactApp/appname2/src/main.jsx
+++ 2.CreateRactApp/appname2/src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Component1 from './component/Component1.jsx'
 
-createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <Component1/>
   </StrictMode>,
 )
